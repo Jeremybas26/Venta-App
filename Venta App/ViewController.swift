e718669204338829ee78683f2b0d176e8bebb8a3
jeremybas26 <jeremybas26@gmail.com>
2022-06-15 20:16:24 -0400

    Added bottom bar with buttons and symbols. Added Title at the top

--- Venta App/ViewController.swift
+++ Venta App/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var homeButton: UIBarButtonItem!
+    @IBOutlet var recepitButton: UIBarButtonItem!
+    @IBOutlet var accountButton: UIBarButtonItem!
+    @IBOutlet var settingsButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func homeScreen() {
+        print("Go to home screen")
+    }
+    
+    @IBAction func recepitScreen() {
+        print("Go to recepit screen")
+    }
+    
+    @IBAction func accountScreen() {
+        print("Go to account screen")
+    }
+    
+    @IBAction func settingsScreen() {
+        print("Go to settings screen")
+    }
 }
 
