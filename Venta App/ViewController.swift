80549a7d9be3149bf6b7d4dbd40711dce166b2ce
jeremybas26 <jeremybas26@gmail.com>
2022-06-16 17:13:53 -0400

    Made Button Switch Work and Added the new tabs

--- Venta App/ViewController.swift
+++ Venta App/ViewController.swift
@@ -8,31 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var homeButton: UIBarButtonItem!
-    @IBOutlet var recepitButton: UIBarButtonItem!
-    @IBOutlet var accountButton: UIBarButtonItem!
-    @IBOutlet var settingsButton: UIBarButtonItem!
 
+    @IBOutlet var groupNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func homeScreen() {
-        print("Go to home screen")
-    }
     
-    @IBAction func recepitScreen() {
-        print("Go to recepit screen")
-    }
     
-    @IBAction func accountScreen() {
-        print("Go to account screen")
-    }
-    
-    @IBAction func settingsScreen() {
-        print("Go to settings screen")
-    }
 }
 
